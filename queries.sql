c5df2b96f590cb050b18d2660eb471486590a266
Cooper Nichols <coopnich92@gmail.com>
2020-06-01 13:38:55 -0500

    5,6,7 done

--- queries.sql
+++ queries.sql
@@ -10,3 +10,16 @@ where i.BillingCountry = "Brazil";
 
 select * from Employee e 
 where e.Title = "Sales Support Agent";
+
+select DISTINCT i.BillingCountry as 'Country List' from Invoice i
+group by i.BillingCountry;
+
+select e.FirstName, e.LastName, e.Title, i.InvoiceId from Employee e 
+left join Invoice i on i.CustomerId = CustomerId
+where e.Title = "Sales Support Agent";
+
+select i.InvoiceId, i.Total, c.FirstName || " " || c.LastName as 'Customer', c.Country, e.FirstName || " " || e.LastName as 'Sales Rep'
+from Customer c 
+left join Invoice i on i.CustomerId = c.CustomerId
+left join Employee e on e.EmployeeId = c.SupportRepId
+order BY i.InvoiceId;
