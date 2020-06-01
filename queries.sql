1b276d2b4e876947ecfcd2c02363f46c006debcf
Cooper Nichols <coopnich92@gmail.com>
2020-06-01 11:45:51 -0500

    part 3 brazil customer invoices

--- queries.sql
+++ queries.sql
@@ -4,3 +4,6 @@ where c.Country != "USA";
 select  c.FirstName, c.LastName, c.Country from Customer c 
 where c.Country = "Brazil";
 
+select i.InvoiceId, i.InvoiceDate, i.BillingCountry, c.FirstName, c.LastName from Invoice i 
+left join Customer c on i.CustomerId = c.CustomerId
+where i.BillingCountry = "Brazil";
